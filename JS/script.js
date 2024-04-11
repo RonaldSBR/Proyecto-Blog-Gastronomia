d10680f8369df3effc77c0a115b48cc84ea12a66
Ronald Stiven Beltran Rojas <ronaldbeltranrojas@gmail.com>
2024-04-10 21:01:06 -0500

    Cambio de texto e imagenes

--- JS/script.js
+++ JS/script.js
@@ -7,16 +7,4 @@ document.getElementById("submitButton").addEventListener("click", function () {
   document.getElementById("instructions").value = "";
 });
 
-const buttondarkmode = document.getElementById("a3");
-const elementbody = document.querySelector("body");
-const container = document.querySelector(".container");
-const originalBackgroundColor = container.style.backgroundColor;
 
-buttondarkmode.addEventListener("click", function () {
-  event.preventDefault();
-  if (document.body.style.backgroundColor !== "black") {
-    document.body.style.backgroundColor = "black";
-  } else {
-    document.body.style.backgroundColor = "#F3E5D8";
-  }
-});
